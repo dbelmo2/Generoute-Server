d923b0ad1786206655d3de95885fba858c92202f
Danny <danny.belmonte4@gmail.com>
2023-05-15 13:54:01 -0500

    Configured cors to accept all requests

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const router = require('./routes/index');
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(router);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
