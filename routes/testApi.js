34258dc4dbac1e9c3b7802f927a65b3f2b46aa17
Danny <danny.belmonte4@gmail.com>
2023-05-28 19:59:39 -0500

    api changes

--- routes/testApi.js
+++ routes/testApi.js
@@ -2,6 +2,8 @@
 const logger = require('../logger');
 const util = require('./util');
 
+const FILE_NAME = 'testApi.js';
+
 const getLandUseDataAPI = async (req, res) => {
   try {
     const result = await util.getLandUseData(req.query.latitude, req.query.longitude);
@@ -23,18 +25,20 @@ const generateCoordAPI = async (req, res) => {
 
 const generateRectangleRouteAPI = async (req, res) => {
   try {
-    logger.info(`Received address: ${req.query.address}`);
+    logger.info(`Received address: ${req.query.address}`, generateRectangleRouteAPI.name, FILE_NAME);
     const startCoords = await util.generateCoord(req.query.address);
     logger.info('start coordinates');
     logger.info(startCoords);
     const kmInMile = 1.60934;
     const lengthInKm = req.query.length * kmInMile;
-    logger.info(`Converted length of ${req.query.length} miles to ${lengthInKm} Km`);
+    logger.info(`Received a request for a route with a total length of ${req.query.length} Miles or ${lengthInKm} KM`);
     logger.info(`Length for each side: ${lengthInKm / 4} KM`);
     const routeData = await util.generateRectangleRoute([startCoords.longitude, startCoords.latitude], lengthInKm / 4, lengthInKm / 4);
+    util.getRouteDistance(routeData.route.geometry.coordinates);
     res.json(routeData);
   } catch (error) {
     logger.info('Error occured while generating rectangle route.');
+    console.log(error);
     res.json({ message: error.message }).status(400);
   }
 };
@@ -48,7 +52,7 @@ const getDistanceAPI = (req, res) => {
     logger.info(`Calculating distance between lat 1: ${latOne} lon 1: ${lonOne}, lat 2: ${latTwo} lon 2: ${lonTwo}`);
     const result = util.getDistanceFromLatLonInKm(latOne, lonOne, latTwo, lonTwo);
     logger.info(`Distance calculated: ${result}`);
-    res.send(result);
+    res.send(`${result}`);
   } catch (error) {
     logger.info(`Error occured while calculating the distance: ${error.message}`);
     res.json({ message: error.message }).status(400);
